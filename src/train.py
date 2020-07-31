df70f559bdf50b7874e15ccfd711684fdd80a120
pwswierczynski <pwswierczynski@gmail.com>
2020-07-30 23:03:58 -0400

    First working version of the training script

---
+++ src/train.py
@@ -0,0 +1,71 @@
+# TODO: Add validation. Move dimension's permutation to the dataset.
+
+""" Configuration """
+PATH_TO_DATE = "../data"
+BATCH_SIZE = 16
+NUM_WORKERS = 2
+SHUFFLE_DATASET = True
+LEARNING_RATE = 0.001
+N_EPOCHS = 20
+MODEL_PATH = "../models"
+
+import os
+import torch
+
+from math import inf
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from networks import UNet
+from dataset import VtkDataset
+
+training_data = VtkDataset(base_dir=PATH_TO_DATE)
+data_loader = DataLoader(
+    training_data,
+    batch_size=BATCH_SIZE,
+    shuffle=SHUFFLE_DATASET,
+    num_workers=NUM_WORKERS,
+)
+
+# Use GPU if available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Create a model and optimization method
+model = UNet()
+model = model.to(device)
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+best_loss = inf
+
+for epoch in range(N_EPOCHS):
+    # monitor training loss
+    train_loss = 0.0
+
+    for idx_batch, batch in tqdm(enumerate(data_loader)):
+
+        # Preprocessing the data
+        geometries, flows = batch["geometry"], batch["flow"]
+        geometries = geometries.permute(0, 3, 1, 2)
+        flows = flows.permute(0, 3, 1, 2)
+
+        # Forward and backward pass for optimization
+        optimizer.zero_grad()
+        outputs = model(geometries)
+        loss = criterion(outputs, flows)
+        loss.backward()
+        optimizer.step()
+
+        # Compute loss
+        train_loss += loss.item() * flows.size(0)
+
+    # Training statistics
+    train_loss = train_loss / len(training_data)
+    print(f"Epoch: {epoch} \tTraining Loss: {train_loss:.6f}")
+
+    # We always store the best model up to the current epoch
+    if train_loss < best_loss:
+        best_loss = train_loss
+        print("Saving the model!")
+        path_to_save = os.path.join(MODEL_PATH, f"model_checkpoint.pt")
+        torch.save(model.state_dict(), path_to_save)

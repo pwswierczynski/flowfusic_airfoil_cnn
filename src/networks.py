17b53c3594ab4a3c5e309512a9bf8e3eead944c9
pwswierczynski <pwswierczynski@gmail.com>
2020-08-02 18:33:32 -0400

    Trying deeper network

--- src/networks.py
+++ src/networks.py
@@ -27,9 +27,11 @@ class UNet(nn.Module):
         self.downscaling_1 = Downscaling(16, 32)
         self.downscaling_2 = Downscaling(32, 64)
         self.downscaling_3 = Downscaling(64, 128)
-        self.upscaling_1 = Upscaling(128, 64)
-        self.upscaling_2 = Upscaling(64, 32)
-        self.upscaling_3 = Upscaling(32, 16)
+        self.downscaling_4 = Downscaling(128, 256)
+        self.upscaling_1 = Upscaling(256, 128)
+        self.upscaling_2 = Upscaling(128, 64)
+        self.upscaling_3 = Upscaling(64, 32)
+        self.upscaling_4 = Upscaling(32, 16)
         self.output_conv = nn.Conv2d(16, self.output_dim, kernel_size=1)
 
     def forward(self, x):
@@ -38,11 +40,13 @@ class UNet(nn.Module):
         x1 = self.input_conv(x)
         x2 = self.downscaling_1(x1)
         x3 = self.downscaling_2(x2)
-        x = self.downscaling_3(x3)
+        x4 = self.downscaling_3(x3)
+        x = self.downscaling_4(x4)
 
         # Upscaling part of UNet
-        x = self.upscaling_1(x, x3)
-        x = self.upscaling_2(x, x2)
-        x = self.upscaling_3(x, x1)
+        x = self.upscaling_1(x, x4)
+        x = self.upscaling_2(x, x3)
+        x = self.upscaling_3(x, x2)
+        x = self.upscaling_4(x, x1)
         output = self.output_conv(x)
         return output

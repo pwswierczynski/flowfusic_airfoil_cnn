d3d1d114a2ee1873f068e346af66ff8f06084844
pwswierczynski <pwswierczynski@gmail.com>
2020-08-05 11:53:08 -0400

    Added domains corners as class variables

--- src/geometry_dataset_creation.py
+++ src/geometry_dataset_creation.py
@@ -13,167 +13,197 @@ from typing import Optional, Tuple
 
 
 class AirfoilGeometrySampler:
-	def __init__(self, n_points: int = 50) -> None:
+    def __init__(
+        self,
+        n_points: int = 50,
+        top_left_corner: Tuple[float, float] = (-1, 0.5),
+        bottom_right_corner: Tuple[float, float] = (2, -0.5),
+    ) -> None:
 
-		# Ranges of parameters describing the airfoil geometry.
-		# Values taken from the original paper.
-		# Camber parameters' ranges
-		self.c_1min, self.c_1max = 0.01, 0.96
-		self.c_2min, self.c_2max = 0.02, 0.97
-		self.c_3min, self.c_3max = -0.074, 0.247
-		self.c_4min, self.c_4max = -0.102, 0.206
+        # Ranges of parameters describing the airfoil geometry.
+        # Values taken from the original paper.
+        # Camber parameters' ranges
+        self.c_1min, self.c_1max = 0.01, 0.96
+        self.c_2min, self.c_2max = 0.02, 0.97
+        self.c_3min, self.c_3max = -0.074, 0.247
+        self.c_4min, self.c_4max = -0.102, 0.206
 
-		# Thickness parameters' ranges
-		self.X_Tmin, self.X_Tmax = 0.2002, 0.4813
-		self.T_min, self.T_max = 0.0246, 0.3227
-		self.rho_min, self.rho_max = 0.175, 1.4944
-		self.beta_min, self.beta_max = 0.1452, 4.8724
+        # Thickness parameters' ranges
+        self.X_Tmin, self.X_Tmax = 0.2002, 0.4813
+        self.T_min, self.T_max = 0.0246, 0.3227
+        self.rho_min, self.rho_max = 0.175, 1.4944
+        self.beta_min, self.beta_max = 0.1452, 4.8724
 
-		self.n_points = n_points
+        self.n_points = n_points
 
-	def _compute_airfoil_geometry(
-		self,
-		c1: float,
-		c2: float,
-		c3: float,
-		c4: float,
-		X_T: float,
-		T: float,
-		rho_bar: float,
-		beta_bar: float,
-	) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        self.top_left_corner = top_left_corner
+        self.bottom_right_corner = bottom_right_corner
 
-		rho = rho_bar * (T / X_T) ** 2
-		beta = beta_bar * np.arctan(T / (1 - X_T))
+    def _compute_airfoil_geometry(
+        self,
+        c1: float,
+        c2: float,
+        c3: float,
+        c4: float,
+        X_T: float,
+        T: float,
+        rho_bar: float,
+        beta_bar: float,
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
 
-		# Camber line
-		x_camber = (
-			3 * c1 * self.n_points * (1 - self.n_points) ** 2
-			+ 3 * c2 * (1 - self.n_points) * self.n_points ** 2
-			+ self.n_points ** 3
-		)
-		y_camber = (
-			3 * c3 * self.n_points * (1 - self.n_points) ** 2
-			+ 3 * c4 * (1 - self.n_points) * self.n_points ** 2
-		)
+        rho = rho_bar * (T / X_T) ** 2
+        beta = beta_bar * np.arctan(T / (1 - X_T))
 
-		# Assemble linear system to solve for thickness parameters
-		A = np.array(
-			[
-				[np.sqrt(X_T), X_T, X_T ** 2, X_T ** 3, X_T ** 4],
-				[0.5 / np.sqrt(X_T), 1, 2 * X_T, 3 * X_T ** 2, 4 * X_T ** 3],
-				[0.25, 0.5, 1, 1.5, 2],
-				[1, 0, 0, 0, 0],
-				[1, 1, 1, 1, 1],
-			]
-		)
-		R = np.array([T, 0, -np.tan(beta / 2), np.sqrt(2 * rho), 0])
+        # Camber line
+        x_camber = (
+            3 * c1 * self.n_points * (1 - self.n_points) ** 2
+            + 3 * c2 * (1 - self.n_points) * self.n_points ** 2
+            + self.n_points ** 3
+        )
+        y_camber = (
+            3 * c3 * self.n_points * (1 - self.n_points) ** 2
+            + 3 * c4 * (1 - self.n_points) * self.n_points ** 2
+        )
 
-		t = np.linalg.solve(A, R)
+        # Assemble linear system to solve for thickness parameters
+        A = np.array(
+            [
+                [np.sqrt(X_T), X_T, X_T ** 2, X_T ** 3, X_T ** 4],
+                [0.5 / np.sqrt(X_T), 1, 2 * X_T, 3 * X_T ** 2, 4 * X_T ** 3],
+                [0.25, 0.5, 1, 1.5, 2],
+                [1, 0, 0, 0, 0],
+                [1, 1, 1, 1, 1],
+            ]
+        )
+        R = np.array([T, 0, -np.tan(beta / 2), np.sqrt(2 * rho), 0])
 
-		thickness = (
-			t[0] * np.sqrt(x_camber)
-			+ t[1] * x_camber
-			+ t[2] * x_camber ** 2
-			+ t[3] * x_camber ** 3
-			+ t[4] * x_camber ** 4
-		)
+        t = np.linalg.solve(A, R)
 
-		upper_curve = y_camber + 0.5 * thickness
-		lower_curve = y_camber - 0.5 * thickness
+        thickness = (
+            t[0] * np.sqrt(x_camber)
+            + t[1] * x_camber
+            + t[2] * x_camber ** 2
+            + t[3] * x_camber ** 3
+            + t[4] * x_camber ** 4
+        )
 
-		return x_camber, upper_curve, lower_curve
+        upper_curve = y_camber + 0.5 * thickness
+        lower_curve = y_camber - 0.5 * thickness
 
-	def sample_airfoil_geometry(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        return x_camber, upper_curve, lower_curve
 
-		c1 = uniform(self.c_1min, self.c_1max)
-		c2 = uniform(self.c_2min, self.c_2max)
-		c3 = uniform(self.c_3min, self.c_3max)
-		c4 = uniform(self.c_4min, self.c_4max)
+    def sample_airfoil_geometry(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
 
-		X_T = uniform(self.X_Tmin, self.X_Tmax)
-		T = uniform(self.T_min, self.T_max)
-		rho_bar = uniform(self.rho_min, self.rho_max)
-		beta_bar = uniform(self.beta_min, self.beta_max)
+        c1 = uniform(self.c_1min, self.c_1max)
+        c2 = uniform(self.c_2min, self.c_2max)
+        c3 = uniform(self.c_3min, self.c_3max)
+        c4 = uniform(self.c_4min, self.c_4max)
 
-		x_camber, upper_curve, lower_curve = self._compute_airfoil_geometry(
-			c1=c1, c2=c2, c3=c3, c4=c4, X_T=X_T, T=T, rho_bar=rho_bar, beta_bar=beta_bar
-		)
-		return x_camber, upper_curve, lower_curve
+        X_T = uniform(self.X_Tmin, self.X_Tmax)
+        T = uniform(self.T_min, self.T_max)
+        rho_bar = uniform(self.rho_min, self.rho_max)
+        beta_bar = uniform(self.beta_min, self.beta_max)
 
-	def create_mesh(self, x_camber: np.ndarray, upper_curve: np.ndarray, lower_curve: np.ndarray) -> None:
+        x_camber, upper_curve, lower_curve = self._compute_airfoil_geometry(
+            c1=c1, c2=c2, c3=c3, c4=c4, X_T=X_T, T=T, rho_bar=rho_bar, beta_bar=beta_bar
+        )
+        return x_camber, upper_curve, lower_curve
 
-		my_mesh = Mesh()
+    def create_mesh(
+        self, x_camber: np.ndarray, upper_curve: np.ndarray, lower_curve: np.ndarray
+    ) -> None:
 
-		top_left = Entity.Point([-1, 0.5, 0])
-		top_right = Entity.Point([2, 0.5, 0])
-		bottom_left = Entity.Point([-1, -0.5, 0])
-		bottom_right = Entity.Point([2, -0.5, 0])
+        my_mesh = Mesh()
 
-		# Add domain's corners to the geometry
-		my_mesh.addEntity(top_left)
-		my_mesh.addEntity(top_right)
-		my_mesh.addEntity(bottom_left)
-		my_mesh.addEntity(bottom_right)
+        top_left = Entity.Point([*self.top_left_corner, 0])
+        top_right = Entity.Point(
+            [self.bottom_right_corner[0], self.top_left_corner[1], 0]
+        )
+        bottom_left = Entity.Point(
+            [self.top_left_corner[0], self.bottom_right_corner[1], 0]
+        )
+        bottom_right = Entity.Point([*self.bottom_right_corner, 0])
 
-		# Define the outer boundary
-		top_boundary = Entity.Curve([top_left, top_right])
-		right_boundary = Entity.Curve([top_right, bottom_right])
-		bottom_boundary = Entity.Curve([bottom_right, bottom_left])
-		left_boundary = Entity.Curve([bottom_left, top_left])
+        # Add domain's corners to the geometry
+        my_mesh.addEntity(top_left)
+        my_mesh.addEntity(top_right)
+        my_mesh.addEntity(bottom_left)
+        my_mesh.addEntity(bottom_right)
 
-		domain_outer_boundary = [top_boundary, right_boundary, bottom_boundary, left_boundary]
-		my_mesh.addEntities(domain_outer_boundary)
+        # Define the outer boundary
+        top_boundary = Entity.Curve([top_left, top_right])
+        right_boundary = Entity.Curve([top_right, bottom_right])
+        bottom_boundary = Entity.Curve([bottom_right, bottom_left])
+        left_boundary = Entity.Curve([bottom_left, top_left])
 
-		outer_curve = Entity.CurveLoop(domain_outer_boundary, mesh=my_mesh)
+        domain_outer_boundary = [
+            top_boundary,
+            right_boundary,
+            bottom_boundary,
+            left_boundary,
+        ]
+        my_mesh.addEntities(domain_outer_boundary)
 
-		# Add airfoil points
-		airfoil_points = []
-		for x, y in zip(x_camber, upper_curve):
-			point = Entity.Point([x, y, 0])
-			airfoil_points.append(point)
-			my_mesh.addEntity(point)
+        outer_curve = Entity.CurveLoop(domain_outer_boundary, mesh=my_mesh)
 
-		for x, y in zip(x_camber[::-1][1:-1], lower_curve[::-1][1:-1]):
-			point = Entity.Point([x, y, 0])
-			airfoil_points.append(point)
-			my_mesh.addEntity(point)
+        # Add airfoil points
+        airfoil_points = []
+        for x, y in zip(x_camber, upper_curve):
+            point = Entity.Point([x, y, 0])
+            airfoil_points.append(point)
+            my_mesh.addEntity(point)
 
-		# Discretize the airfoil profile
-		intervals = []
-		for i in range(len(airfoil_points) - 1):
-			point1 = airfoil_points[i]
-			point2 = airfoil_points[i + 1]
-			interval = Entity.Curve([point1, point2])
-			intervals.append(interval)
+        for x, y in zip(x_camber[::-1][1:-1], lower_curve[::-1][1:-1]):
+            point = Entity.Point([x, y, 0])
+            airfoil_points.append(point)
+            my_mesh.addEntity(point)
 
-		# Close the profile using endpoints
-		point1 = airfoil_points[-1]
-		point2 = airfoil_points[0]
-		interval = Entity.Curve([point1, point2])
-		intervals.append(interval)
+        # Discretize the airfoil profile
+        intervals = []
+        for i in range(len(airfoil_points) - 1):
+            point1 = airfoil_points[i]
+            point2 = airfoil_points[i + 1]
+            interval = Entity.Curve([point1, point2])
+            intervals.append(interval)
 
-		my_mesh.addEntities(intervals)
-		airfoil_profile = Entity.CurveLoop(intervals, mesh=my_mesh)
+        # Close the profile using endpoints
+        point1 = airfoil_points[-1]
+        point2 = airfoil_points[0]
+        interval = Entity.Curve([point1, point2])
+        intervals.append(interval)
 
-		Entity.PlaneSurface([airfoil_profile, outer_curve], mesh=my_mesh)
+        my_mesh.addEntities(intervals)
+        airfoil_profile = Entity.CurveLoop(intervals, mesh=my_mesh)
 
-		# adding Coherence option
-		my_mesh.Coherence = True
+        Entity.PlaneSurface([airfoil_profile, outer_curve], mesh=my_mesh)
 
-		my_mesh.writeGeo('my_mesh.geo')
-		subprocess.run(["gmsh", "my_mesh.geo", "-2", "-o", "newer_mesh.stl", "-format", "stl", "-save_all"])
+        # adding Coherence option
+        my_mesh.Coherence = True
 
-	def plot_airfoil(
-		self, x_camber, upper_curve, lower_curve, filename: Optional[str] = None
-	):
+        my_mesh.writeGeo("my_mesh.geo")
+        subprocess.run(
+            [
+                "gmsh",
+                "my_mesh.geo",
+                "-2",
+                "-o",
+                "newer_mesh.stl",
+                "-format",
+                "stl",
+                "-save_all",
+            ]
+        )
 
-		plt.figure()
-		plt.plot(x_camber, lower_curve)
-		plt.plot(x_camber, upper_curve)
-		plt.gca().set_aspect("equal", adjustable="box")
-		plt.xlim(0, 1)
-		plt.ylim(-0.25, 0.25)
-		if filename is not None:
-			plt.savefig(filename)
-		plt.show()
+    def plot_airfoil(
+        self, x_camber, upper_curve, lower_curve, filename: Optional[str] = None
+    ):
+
+        plt.figure()
+        plt.plot(x_camber, lower_curve)
+        plt.plot(x_camber, upper_curve)
+        plt.gca().set_aspect("equal", adjustable="box")
+        plt.xlim(0, 1)
+        plt.ylim(-0.25, 0.25)
+        if filename is not None:
+            plt.savefig(filename)
+        plt.show()
